98447c41bb64570db17fe397a67210acde0b8de2
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-11-15 12:29:29 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,4 +1,21 @@
-        (
+#!/bin/bash
+function git_clone() {
+  git clone --depth 1 $1 $2 || true
+ }
+function git_sparse_clone() {
+  branch="$1" rurl="$2" localdir="$3" && shift 3
+  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+  cd $localdir
+  git sparse-checkout init --cone
+  git sparse-checkout set $@
+  mv -n $@ ../
+  cd ..
+  rm -rf $localdir
+  }
+function mvdir() {
+mv -n `find $1/* -maxdepth 0 -type d` ./
+rm -rf $1
+}
         git_clone https://github.com/kiddin9/my-packages && mvdir my-packages
         git_clone https://github.com/kiddin9/openwrt-bypass && mvdir openwrt-bypass
         git_clone https://github.com/kiddin9/luci-app-dnsfilter
